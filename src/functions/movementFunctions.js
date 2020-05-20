401a95af4dc04cf7a2512de22645e05695e75edb
AnnSo-B <as.baujard.33@gmail.com>
2020-05-20 15:52:38 +0200

    add treasure and victory naotion

--- src/functions/movementFunctions.js
+++ src/functions/movementFunctions.js
@@ -44,7 +44,7 @@ export const getNextPosition = (oldPosition, direction) => {
 };
 
 // determine if the next tile is passable and return the position
-export const getNewPosition = (oldPosition, nextPosition) => {
+export const getNextTileValue = (nextPosition) => {
   // we need to get the coordinate corresponding to the next position
   // the new position is a pixel size so we need to divide it by the
   // sprite size to get the coordinate in the map
@@ -52,13 +52,18 @@ export const getNewPosition = (oldPosition, nextPosition) => {
   const nextTileY = nextPosition[1] / SPRITE_SIZE;
   // we retrieve the value of this tile
   const nextTileValue = tiles[nextTileY][nextTileX];
-  // if its value is higner than 5, it's impassable, so the player stays
-  // where he was
-  if (nextTileValue > 5) {
-    return oldPosition;
-  }
-  // otherwise he goes to the next position
-  return nextPosition;
+
+  return nextTileValue;
+  // // if its value is higner than 5, it's impassable, so the player stays
+  // // where he was
+  // if (nextTileValue > 5) {
+  //   return oldPosition;
+  // }
+  // // eslint-disable-next-line no-else-return
+  // else {
+  //   // otherwise he goes to the next position
+  //   return nextPosition;
+  // }
 };
 
 // determine which sprite we'll be displayed next according to player's movement
--- src/reducers/game.js
+++ src/reducers/game.js
@@ -2,7 +2,7 @@
 
 // local imports
 import { HANDLE_MOVEMENT } from '../actions';
-import { getNextPosition, getNewPosition, getSpriteLocation } from '../functions/movementFunctions';
+import { getNextPosition, getNextTileValue, getSpriteLocation } from '../functions/movementFunctions';
 import tiles from '../data/mapData';
 
 // state
@@ -10,8 +10,9 @@ const initialState = {
   tiles,
   position: [0, 0],
   spriteLocation: '0px 0px',
-  direction: 'East',
+  // direction: 'East',
   walkIndex: 0,
+  victory: false,
 };
 
 // reducer
@@ -21,15 +22,21 @@ export default (state = initialState, action = {}) => {
       // eslint-disable-next-line no-case-declarations
       const nextPosition = getNextPosition(state.position, action.payload);
       // eslint-disable-next-line no-case-declarations
-      const newPosition = getNewPosition(state.position, nextPosition);
+      const nextTileValue = getNextTileValue(nextPosition);
       // eslint-disable-next-line no-case-declarations
-      const newSpriteLocation = getSpriteLocation(state.spriteLocation, action.payload, state.walkIndex);
+      const newSpriteLocation = getSpriteLocation(
+        state.spriteLocation,
+        action.payload,
+        state.walkIndex,
+      );
       return {
         ...state,
-        position: newPosition,
+        position: nextTileValue < 6 ? nextPosition : state.position,
         spriteLocation: newSpriteLocation,
         // since we have 8 index to our sprite, when it's higher than 7, we need to go back to 0
         walkIndex: state.walkIndex + 1 > 7 ? 0 : state.walkIndex + 1,
+        // if the tile is the treasure, the player wins
+        victory: nextTileValue === 2,
       };
     default:
       return state;
